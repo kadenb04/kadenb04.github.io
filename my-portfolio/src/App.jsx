36f95b4bb4e831a9f40d1dd2737e1cf9745f2846
Kaden Buchanan <kadenbuchanan04@gmail.com>
2026-02-24 17:20:18 -0700

    add Intro to the App

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Intro from "./components/Intro"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>My Portfolio!</h1>
+      <Intro />
     </>
   )
 }
